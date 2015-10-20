518e247ce9097154a478d5abd7b77b75eb8ca06c
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-10-20 09:57:32 -0700

    use event listener

--- ryan.js
+++ ryan.js
@@ -1,11 +1,13 @@
-function scrambler(str) {
-    var thingy = str.textContent;
-    var that = thingy.split("");
-    var len = that.length;
-    var thatChar;
-    while (len) {
-        var randomNum = Math.random() * len-- | 0;
-        thatChar = that[len], that[len] = that[randomNum], that[randomNum] = thatChar;
+window.addEventListener('click',
+    function scrambler(str) {
+        var thingy = str.textContent;
+        var that = thingy.split("");
+        var len = that.length;
+        var thatChar;
+        while (len) {
+            var randomNum = Math.random() * len-- | 0;
+            thatChar = that[len], that[len] = that[randomNum], that[randomNum] = thatChar;
+        }
+        document.getElementById("moniker").textContent = that.join("");
     }
-    document.getElementById("moniker").textContent = that.join("");
-}
+);
