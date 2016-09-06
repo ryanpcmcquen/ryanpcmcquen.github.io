dbc8ad8b2a0bcd6229585910611808ae7c29791d
Ryan P.C. McQuen <ryan.q@linux.com>
2016-09-06 08:31:45 -0700

    Add underline to playing song.

--- js/songz.js
+++ js/songz.js
@@ -3,5 +3,9 @@ const currentSong = document.querySelector('#currentSong');
 songs.map((s) => {
   s.addEventListener('click', () => {
     currentSong.setAttribute('src', "https://archive.org/download/plays_some_standards/" + s.textContent.toLowerCase().replace(/\s/g, '_') + ".mp3");
+    songs.map((i) => {
+      i.style.textDecoration = 'none';
+    });
+    s.style.textDecoration = 'underline';
   });
 });
