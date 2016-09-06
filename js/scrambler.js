d8b681cc006c356a20585c74a9b2cbf58efe8837
Ryan P.C. McQuen <ryan.q@linux.com>
2016-09-06 09:18:21 -0700

    Optimize scrambling.

--- js/scrambler.js
+++ js/scrambler.js
@@ -1,11 +1,11 @@
-(() => {
+document.addEventListener('DOMContentLoaded', () => {
+  let moniker = document.getElementById("moniker");
   const scrambler = () => {
-    const moniker = document.getElementById("moniker");
     let thingy = moniker.textContent;
     const thatArr = thingy.split('');
-    thingy = thatArr.sort(() => {
+    moniker.textContent = thatArr.sort(() => {
       return 1 - Math.random();
     }).join('');
   };
   window.addEventListener('click', scrambler);
-})();
+});
