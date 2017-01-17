0c2e6d251f092a203e38839b0844e2ccdaa7ba56
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-01-17 09:33:44 -0800

    Add LibreJS stuff.

--- js/songz.js
+++ js/songz.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:cf05388f2679ee054f2beb29a391d25f4e673ac3&dn=gpl-2.0.txt GPL-v2-or-later
 const songs = [...document.querySelectorAll('.songList li')];
 const currentSong = document.querySelector('#currentSong');
 songs.map((s) => {
@@ -10,3 +11,4 @@ songs.map((s) => {
     currentSong.play();
   });
 });
+// @license-end
